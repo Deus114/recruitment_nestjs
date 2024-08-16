3ed2370192d85ceb352089c664b738f78b2f67bd
DESKTOP-24GU9HH\ASUS <lekhaduy0917880045@gmail.com>
2024-08-16 19:44:45 +0700

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    // MongooseModule.forRoot('mongodb+srv://lekhaduy0917880045:JZmWyMTWZaOxLTfZ@cluster0.8qddgev.mongodb.net/'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UsersModule,
+
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
