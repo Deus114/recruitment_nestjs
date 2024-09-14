84714dec7aa7c54766f01b4ce6d52ff8603a9a13
DESKTOP-24GU9HH\ASUS <lekhaduy0917880045@gmail.com>
2024-09-14 15:22:21 +0700

    setup JWT

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,65 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import mongoose, { Model } from 'mongoose';
+import { genSaltSync, hashSync, compareSync } from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>
+  ) { }
+
+  getHashPassword = (password: string) => {
+    var salt = genSaltSync(10);
+    var hash = hashSync(password, salt);
+    return hash;
+  }
+
+  async create(createUserDto: CreateUserDto) {
+    let hashPassword = this.getHashPassword(createUserDto.password);
+    let user = await this.userModel.create({
+      email: createUserDto.email, password: hashPassword, name: createUserDto.name
+    })
+    return user;
   }
 
   findAll() {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    if (!mongoose.Types.ObjectId.isValid(id))
+      return 'Không tìm thấy người dùng.';
+
+    return this.userModel.findOne({
+      _id: id
+    });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  findOneByUserName(username: string) {
+    return this.userModel.findOne({
+      email: username
+    })
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  isValidPassword(password: string, hash: string) {
+    return compareSync(password, hash);
+  }
+
+  async update(updateUserDto: UpdateUserDto) {
+    return await this.userModel.updateOne({ _id: updateUserDto._id }, { ...updateUserDto });
+  }
+
+  remove(id: string) {
+    if (!mongoose.Types.ObjectId.isValid(id))
+      return 'Không tìm thấy người dùng.';
+
+    return this.userModel.deleteOne({
+      _id: id
+    });
   }
 }
